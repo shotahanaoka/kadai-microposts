4c195fad43134a02c0be897a13a34d7f98082ce9
shota.hanaoka <ec2-user@ip-172-31-11-154.us-east-2.compute.internal>
2018-06-14 01:05:17 +0000

    favorite function

--- app/User.php
+++ app/User.php
@@ -42,6 +42,11 @@ class User extends Authenticatable
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
     }
     
+    public function favoritings()
+    {
+        return $this->belongsToMany(Micropost::class, 'user_favorite', 'user_id', 'favorite_id')->withTimestamps();
+    } 
+    
     public function follow($userId)
 {
     // confirm if already following
@@ -78,9 +83,55 @@ public function unfollow($userId)
 }
 
 
+
+
 public function is_following($userId) {
     return $this->followings()->where('follow_id', $userId)->exists();
 }
+
+public function favorite($micropostId)
+{
+    // confirm if already following
+    $exist = $this->is_favoriting($micropostId);
+    // confirming that it is not you
+    $its_me = $this->id == $micropostId;
+
+    if ($exist /*|| $its_me*/) {
+        // do nothing if already following
+        return false;
+    } else {
+        // follow if not following
+        $this->favoritings()->attach($micropostId);
+        return true;
+    }
+}
+
+public function unfavorite($micropostId)
+{
+    // confirming if already following
+    $exist = $this->is_favoriting($micropostId);
+    // confirming that it is not you
+    $its_me = $this->id == $micropostId;
+
+
+    if ($exist /*&& !$its_me*/) {
+        // stop following if following
+        $this->favoritings()->detach($micropostId);
+        return true;
+    } else {
+        // do nothing if not following
+        return false;
+    }
+}
+
+
+public function is_favoriting($micropostId) {
+    return $this->favoritings()->where('favorite_id', $micropostId)->exists();
+}
+
+
+
+
 
 public function feed_microposts()
     {
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -13,10 +13,12 @@
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
             </div>
             <div>
+                @include('user_favorite.favorite_button', ['user' => $user], ['class' => 'btn btn-default btn-xs'])
+
                 @if (Auth::user()->id == $micropost->user_id)
-                    {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
-                    {!! Form::close() !!}
+                {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                {!! Form::close() !!}
                 @endif
             </div>
         </div>
